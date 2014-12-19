6ea4084fae47fc04da2cfd684f55f0497116b4cf
Glenn Goodrich <glenn.goodrich@gmail.com>
2014-12-19 08:28:03 -0500

    Fix specs, add 2.0-rc1 jars

--- lib/orientdb/oclass.rb
+++ lib/orientdb/oclass.rb
@@ -102,7 +102,7 @@ module OrientDB
             klass = db.schema.create_class name, use_cluster
           elsif add_cluster && !db.storage.cluster_names.include?(name.downcase)
             #debugger
-            cluster = db.storage.add_cluster STORAGE_TYPES[:physical], name.downcase, "/tmp/database", 'default', false, {}
+            cluster = db.storage.add_cluster name.downcase, false, {}
             klass   = db.schema.create_class name, cluster
           else
             klass = db.schema.create_class name
